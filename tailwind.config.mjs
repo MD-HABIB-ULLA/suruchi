cb30ed979cd0dd163afa1c2f20b3fd7a4c5e7a68
habib ulla <habibulla1278@gmail.com>
2024-12-31 09:04:31 +0600

    first commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,9 +7,21 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        jost: ["Jost", "serif"], // Adding custom Jost font
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "var(--background)", // Background color
+        foreground: "var(--foreground)", // Foreground color
+        primary: "var(--primary-color)", // Primary color
+        secondary: "var(--secondary-color)", // Secondary color
+        muted: "var(--muted)", // Muted color (if defined in CSS)
+        error: "var(--error)", // Error color (if defined in CSS)
+        darkBackground: "var(--dark-background)", // Dark mode background
+        darkForeground: "var(--dark-foreground)", // Dark mode foreground
+        darkTextColor: "var(--dark-text-color)", // Dark mode text color
+        darkBorderColor: "var(--dark-border-color)", // Dark mode border color
+        
       },
     },
   },
